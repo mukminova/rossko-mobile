6417e256aaf6b7ee86afefa9b25ffb0fe6d94bbe
Li <li@mg.local>
2018-08-30 15:05:21 +0300

    init

---
+++ rossko-mobile/rossko-mobile/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  rossko-mobile
+//
+//  Created by Li on 30.08.2018.
+//  Copyright © 2018 Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
